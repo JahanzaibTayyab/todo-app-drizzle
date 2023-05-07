6ec3ff358134feb073c495db18b150d05148169a
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2023-05-07 12:55:25 +0500

    drizzle integration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    PGHOST: "ep-red-sunset-577822.us-east-2.aws.neon.tech",
-    PGDATABASE: "neondb",
-    PGUSER: "zahid-frooq",
-    PGPASSWORD: "j6xyKP2XQmUA",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
